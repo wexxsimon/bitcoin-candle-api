07b9728dd4c6a39525b5e3e474a318225aaf9c15
Wesley Simões <wexxcode@gmail.com>
2023-08-26 22:45:44 -0300

    Commit inicial

---
+++ src/messages/messageChannel.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv'
+import { Channel, connect } from 'amqplib'
+
+const createMessageChannel = async (): Promise<Channel> => {
+    config()
+
+    try {
+        const connection = await connect(process.env.AMQP_SERVER)
+        const channel = await connection.createChannel()
+        await channel.assertQueue(process.env.QUEUE_NAME)
+        console.log('Connected to RabbitMQ')
+
+        return channel
+    } catch (err) {
+        console.log('Error while trying to connect to RabbitMQ')
+        console.log(err)
+        return null
+    }
+}
+
+export default createMessageChannel
